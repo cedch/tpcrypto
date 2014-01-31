e376e2f322c116c73106e203acada315cd7cbf23
Yohann Berthon <yohann.berthon@gmail.com>
2014-01-31 23:33:43 +0100

    i2c works. Prochaine étape petit refacto en classe pcq ca commence a être le bordèle.

--- tp.php
+++ tp.php
@@ -44,18 +44,32 @@ function c2i($string){
 	return $indice;
 }		
 
-function i2c($indice){
+function i2c($indice = 1, $nbLettreMin = 5, $nbLettreMax = 8){
+    $indiceperso = array("a"=>0,"b"=>1,"c"=>2,"d"=>3,"e"=>4,"f"=>5,"g"=>6,"h"=>7,"i"=>8,"j"=>9,"k"=>10,"l"=>11,"m"=>12,"n"=>13,"o"=>14,"p"=>15,"q"=>16,"r"=>17,"s"=>18,"t"=>19,"u"=>20,"v"=>21,"w"=>22,"x"=>23,"y"=>24,"z"=>25,"0"=>26,"1"=>27,"2"=>28,"3"=>29,"4"=>30,"5"=>31,"6"=>32,"7"=>33,"8"=>34,"9"=>35);
 
-	$string = "";
+    $indiceLettre = array();
+    $texteClair = "";
+    $nbLettre = $nbLettreMin;
+    do{
+        if($indice < pow(36, $nbLettre)) {
+            for($j=0;$j<$nbLettre;$j++) {
+                $texteClair = array_search($indice%36, $indiceperso).$texteClair;
+                $indice = (int)($indice/36);
+            }
+        }
+        else {
+            $indice = $indice - pow(36, $nbLettre);
+        }
+        $nbLettre++;
+    }while($texteClair=="" && $nbLettre<=$nbLettreMax);
 
+	return $texteClair;
 
-
-	return $string;
-	
 }
 
 echo c2i($string);
-echo i2c(1);
+echo('<br/>')
+echo i2c(14);
 
 
 ?>
